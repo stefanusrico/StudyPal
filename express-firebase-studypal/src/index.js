5c70d6eb167959769096bf504df7f3726a6f8698
stefanusrico <stefanus.rico70@gmail.com>
2024-06-08 23:17:33 +0700

    update: final group feature

--- express-firebase-studypal/src/index.js
+++ express-firebase-studypal/src/index.js
@@ -288,16 +288,15 @@ app.get("/messages/:groupId", async (req, res) => {
       .collection("group-conversations")
       .doc(groupId)
       .collection("messages")
-      .orderBy("sentAt", "desc")
+      .orderBy("timestamp", "desc")
       .get()
 
     const messages = []
 
     for (const doc of messagesSnapshot.docs) {
       const messageData = doc.data()
-      const senderId = messageData.sentBy
+      const senderId = messageData.senderId
 
-      // Query koleksi users untuk mendapatkan firstName berdasarkan email
       const userSnapshot = await db
         .collection("users")
         .where("email", "==", senderId)
@@ -322,39 +321,232 @@ app.get("/messages/:groupId", async (req, res) => {
   }
 })
 
+app.get("/groups/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId
+
+    const groupsSnapshot = await db.collection("group-conversations").get()
+    const groups = groupsSnapshot.docs
+      .map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+      .filter(
+        (group) =>
+          group.participantIds && !group.participantIds.includes(userId)
+      )
+
+    res.status(200).json(groups)
+  } catch (error) {
+    console.error("Error getting groups:", error)
+    res.status(500).json({ error: "Failed to get groups" })
+  }
+})
+
+app.get("/group/:groupId", async (req, res) => {
+  try {
+    const groupId = req.params.groupId
+
+    const groupDoc = await db
+      .collection("group-conversations")
+      .doc(groupId)
+      .get()
+
+    if (!groupDoc.exists) {
+      return res.status(404).json({ error: "Group not found" })
+    }
+
+    const groupData = groupDoc.data()
+
+    const participantIds = groupData.participantIds || []
+    const userDocs = await Promise.all(
+      participantIds.map((userId) => db.collection("users").doc(userId).get())
+    )
+    const userData = userDocs.map((userDoc) => ({
+      id: userDoc.id,
+      ...userDoc.data(),
+    }))
+
+    const responseData = {
+      ...groupData,
+      userData,
+    }
+
+    res.status(200).json(responseData)
+  } catch (error) {
+    console.error("Error getting group data:", error)
+    res.status(500).json({ error: "Failed to get group data" })
+  }
+})
+
+app.get("/groups/joined/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId
+
+    const groupsSnapshot = await db.collection("group-conversations").get()
+    const groups = groupsSnapshot.docs
+      .map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+      .filter(
+        (group) => group.participantIds && group.participantIds.includes(userId)
+      )
+
+    res.status(200).json(groups)
+  } catch (error) {
+    console.error("Error getting joined groups:", error)
+    res.status(500).json({ error: "Failed to get joined groups" })
+  }
+})
+
+app.post("/create-group", async (req, res) => {
+  try {
+    const { groupName, initiatedBy, description, maxParticipants } = req.body
+
+    const groupRef = db.collection("group-conversations").doc()
+    const groupData = {
+      groupname: groupName,
+      documentId: groupRef.id,
+      description: description,
+      initiatedAt: admin.firestore.FieldValue.serverTimestamp(),
+      initiatedBy: initiatedBy,
+      lastMessage: null,
+      lastUpdatedAt: admin.firestore.FieldValue.serverTimestamp(),
+      participantIds: [initiatedBy],
+      maxParticipants: maxParticipants,
+    }
+
+    await groupRef.set(groupData)
+
+    const userSnapshot = await db
+      .collection("users")
+      .where("email", "==", initiatedBy)
+      .limit(1)
+      .get()
+
+    if (!userSnapshot.empty) {
+      const userData = userSnapshot.docs[0].data()
+      const participantData = {
+        userId: userData.email,
+        imageUrl: userData.imageUrl || "",
+        email: userData.email,
+        name: userData.first_name + " " + userData.last_name,
+      }
+
+      const participantRef = groupRef
+        .collection("participants")
+        .doc(initiatedBy)
+      await participantRef.set(participantData)
+    }
+
+    res
+      .status(200)
+      .json({ groupId: groupRef.id, message: "Group created successfully" })
+  } catch (error) {
+    console.error("Error creating group:", error)
+    res.status(500).json({ error: "Failed to create group" })
+  }
+})
+
+// Endpoint untuk bergabung dengan grup
+app.post("/groups/:groupId/join", async (req, res) => {
+  try {
+    const { groupId } = req.params
+    const { userId } = req.body
+
+    // Periksa apakah grup dengan groupId yang diberikan ada
+    const groupRef = db.collection("group-conversations").doc(groupId)
+    const groupDoc = await groupRef.get()
+
+    if (!groupDoc.exists) {
+      return res.status(404).json({ error: "Group not found" })
+    }
+
+    const groupData = groupDoc.data()
+
+    const participantSnapshot = await groupRef
+      .collection("participants")
+      .where("userId", "==", userId)
+      .limit(1)
+      .get()
+
+    if (!participantSnapshot.empty) {
+      return res.status(400).json({ error: "User already joined the group" })
+    }
+
+    const currentParticipantIds = groupData.participantIds || []
+    const maxParticipants = groupData.maxParticipants || 0
+    if (currentParticipantIds.length >= maxParticipants) {
+      return res
+        .status(400)
+        .json({ error: "Maximum number of participants reached" })
+    }
+
+    const userSnapshot = await db
+      .collection("users")
+      .where("email", "==", userId)
+      .limit(1)
+      .get()
+
+    if (userSnapshot.empty) {
+      return res.status(404).json({ error: "User not found" })
+    }
+
+    const userData = userSnapshot.docs[0].data()
+    const participantData = {
+      userId: userData.email,
+      imageUrl: userData.imageUrl || "",
+      email: userData.email,
+      name: userData.first_name + " " + userData.last_name,
+    }
+
+    const participantRef = groupRef.collection("participants").doc(userId)
+    await participantRef.set(participantData)
+
+    await groupRef.update({
+      participantIds: admin.firestore.FieldValue.arrayUnion(userId),
+    })
+
+    res.status(200).json({ message: "User joined the group successfully" })
+  } catch (error) {
+    console.error("Error joining group:", error)
+    res.status(500).json({ error: "Failed to join group" })
+  }
+})
+
 io.on("connection", (socket) => {
   console.log("New client connected")
 
   socket.on("message", async (data) => {
     try {
-      const { senderId, text } = data
-      const groupId = "group1" //hardcode nyoba dulu brok
+      const { senderId, text, groupId } = JSON.parse(data)
 
       const groupRef = db.collection("group-conversations").doc(groupId)
       const messageData = {
         message: text,
-        sentBy: senderId,
-        sentAt: admin.firestore.FieldValue.serverTimestamp(),
+        senderId: senderId,
+        recipientId: null,
+        status: "sent",
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
       }
 
       await db.runTransaction(async (transaction) => {
         const groupDoc = await transaction.get(groupRef)
         if (!groupDoc.exists) {
-          transaction.set(groupRef, {
-            groupname: "Group 1",
-            initiatedAt: admin.firestore.FieldValue.serverTimestamp(),
-            initiatedBy: senderId,
-            lastMessage: messageData,
-            lastUpdatedAt: admin.firestore.FieldValue.serverTimestamp(),
-            participantIds: [senderId],
-          })
-        } else {
-          // Jika dokumen grup sudah ada, perbarui lastMessage dan lastUpdatedAt
-          transaction.update(groupRef, {
-            lastMessage: messageData,
-            lastUpdatedAt: admin.firestore.FieldValue.serverTimestamp(),
-          })
+          throw new Error("Group not found")
         }
+
+        const lastMessage = {
+          message: text,
+          senderId: senderId,
+          timestamp: admin.firestore.FieldValue.serverTimestamp(),
+        }
+
+        transaction.update(groupRef, {
+          lastMessage: lastMessage,
+          lastUpdatedAt: admin.firestore.FieldValue.serverTimestamp(),
+        })
 
         transaction.set(groupRef.collection("messages").doc(), messageData)
       })
