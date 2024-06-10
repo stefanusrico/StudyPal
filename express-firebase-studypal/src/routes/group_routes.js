7bf3389e41a4e1c260ca7a854df9540aec62cb51
stefanusrico <stefanus.rico70@gmail.com>
2024-06-10 08:42:01 +0700

    add: endpoint totalGroupTime

--- express-firebase-studypal/src/routes/group_routes.js
+++ express-firebase-studypal/src/routes/group_routes.js
@@ -4,6 +4,10 @@ const groupController = require("../controllers/group_controller")
 
 router.get("/groups/:userId", groupController.getGlobalGroup)
 router.get("/group/:groupId", groupController.getGroupData)
+router.get(
+  "/group/:groupId/daily-accumulated-time",
+  groupController.groupTotalTime
+)
 router.get("/groups/joined/:userId", groupController.getJoinedGroup)
 router.post("/create-group", groupController.createGroup)
 router.post("/groups/:groupId/join", groupController.joinGroup)
